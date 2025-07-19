987964b2cec4fd0e776c6e4784d829816d11c4b5
DevLekh <devlekh@Mac-Mini.local>
2025-07-19 15:51:26 +0545

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,9 +91,11 @@
     </div>
 
     <!-- Navbar with Mega Menu -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-white py-3">
+    <nav class="navbar navbar-expand-lg py-0">
         <div class="container">
-            <a class="navbar-brand fw-bold text-primary fs-4" href="#">E.A.T.H Travel</a>
+            <a class="navbar-brand fw-bold text-primary fs-4" href="#">
+              <img src="/images/logo.png" alt="" height="70">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
