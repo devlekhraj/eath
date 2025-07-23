cedcdc27be499616eaf50da74e1d9b2261d04d49
DevLekh <devlekh@Mac-Mini.local>
2025-07-23 23:57:12 +0545

    wip

--- resources/admin/pages/packages/form_section/FormRight.vue
+++ resources/admin/pages/packages/form_section/FormRight.vue
@@ -1,128 +1,130 @@
 <template>
     <div>
-        <v-card class="elevation-0">
-            <v-card-title class="d-flex align-center justify-space-between py-4">
-                <h2 class="font-medium">Itineraries</h2>
-                <v-btn color="primary" size="small" title="Add Itinerary" icon @click="handleOpen()"><v-icon>mdi-plus</v-icon></v-btn>
-            </v-card-title>
-            <v-divider></v-divider>
-            <v-expansion-panels multiple elevation="1">
-                <v-expansion-panel v-for="(itinerary, index) in travelPackage?.itineraries" :key="index">
-                    <v-expansion-panel-title>
-                        <div class="font-weight-medium">
-                            Day {{ index + 1 }} - {{ itinerary.title }}
-                        </div>
-                    </v-expansion-panel-title>
-
-                    <v-expansion-panel-text>
-                        <div>
-                            <div class="mb-3">
-                                <v-btn color="primary" @click="handleOpen(itinerary)"
-                                    variant="tonal"><v-icon>mdi-pencil-box</v-icon> Edit Item</v-btn>
-
+        <div>
+            <v-card class="elevation-0">
+                <v-card-title class="d-flex align-center justify-space-between py-4">
+                    <h2 class="font-medium">Itineraries</h2>
+                    <v-btn color="primary" size="small" title="Add Itinerary" icon @click="handleOpen()"><v-icon>mdi-plus</v-icon></v-btn>
+                </v-card-title>
+                <v-divider></v-divider>
+                <v-expansion-panels multiple elevation="1">
+                    <v-expansion-panel v-for="(itinerary, index) in travelPackage?.itineraries" :key="index">
+                        <v-expansion-panel-title>
+                            <div class="font-weight-medium">
+                                Day {{ index + 1 }} - {{ itinerary.title }}
                             </div>
-                            <div v-html="itinerary.description" class="text-body-2"></div>
-                        </div>
-                    </v-expansion-panel-text>
-                </v-expansion-panel>
-            </v-expansion-panels>
-        </v-card>
-    </div>
-    <div class="mt-4">
-        <v-card class="elevation-0">
-            <v-card-title class="d-flex align-center justify-space-between py-4">
-                <h2 class="font-medium">Included Items</h2>
-                <v-btn color="primary" size="small" icon @click="editItem({}, false)">
-                    <v-icon>mdi-plus</v-icon>
-                </v-btn>
-            </v-card-title>
-
-
-            <v-card-text>
-                <div v-if="travelPackage?.inclusions?.length">
-                    <v-list density="compact" class="mb-4">
-                        <v-list-item v-for="(item, index) in travelPackage?.inclusions" :key="'inc-' + item.id"
-                            class="py-4 position-relative border mb-4 rounded">
-                            <!-- Top right action buttons -->
-                            <div class="position-absolute top-0 right-0 mt-1 mr-1 d-flex">
-                                <v-btn icon size="x-small" variant="tonal" color="primary"
-                                    @click="editItem(item, false)">
-                                    <v-icon>mdi-pencil</v-icon>
-                                </v-btn>
-                            </div>
-
-                             <v-list-item-content>
-                                <div class="d-flex align-start">
-                                    <v-icon color="success" size="20" class="mt-1 mr-3">
-                                        mdi-check-outline
-                                    </v-icon>
-
-                                    <div>
-                                        <div class="font-weight-medium">{{ item.title }}</div>
-                                        <div class="text-body-2 text-medium-emphasis">{{ item.description }}</div>
-                                    </div>
+                        </v-expansion-panel-title>
+    
+                        <v-expansion-panel-text>
+                            <div>
+                                <div class="mb-3">
+                                    <v-btn color="primary" @click="handleOpen(itinerary)"
+                                        variant="tonal"><v-icon>mdi-pencil-box</v-icon> Edit Item</v-btn>
+    
                                 </div>
-                            </v-list-item-content>
-
-                        </v-list-item>
-                    </v-list>
-
-
-                </div>
-                <div v-else class="pl-4">
-                    <p>Items not added yet</p>
-                </div>
-            </v-card-text>
-
-        </v-card>
-    </div>
-    <div class="mt-4">
-        <v-card class="elevation-0">
-            <v-card-title class="d-flex align-center justify-space-between py-4">
-                <h2 class="font-medium">Excluded Items</h2>
-                <v-btn color="primary" size="small" icon @click="editItem({}, true)">
-                    <v-icon>mdi-plus</v-icon></v-btn>
-            </v-card-title>
-
-
-            <v-card-text>
-                <div v-if="travelPackage?.exclusions?.length">
-                    <v-list density="compact" class="mb-4">
-                        <v-list-item v-for="(item, index) in travelPackage?.exclusions" :key="'inc-' + item.id"
-                            class="py-4 position-relative border mb-4 rounded">
-                            <!-- Top right action buttons -->
-                            <div class="position-absolute top-0 right-0 mt-1 mr-1 d-flex">
-                                <v-btn icon size="x-small" variant="tonal" color="primary"
-                                    @click="editItem(item, true)">
-                                    <v-icon>mdi-pencil</v-icon>
-                                </v-btn>
+                                <div v-html="itinerary.description" class="text-body-2"></div>
                             </div>
-
-                            <v-list-item-content>
-                                <div class="d-flex align-start">
-                                    <v-icon color="error" size="20" class="mt-1 mr-3">
-                                        mdi-close-outline
-                                    </v-icon>
-
-                                    <div>
-                                        <div class="font-weight-medium">{{ item.title }}</div>
-                                        <div class="text-body-2 text-medium-emphasis">{{ item.description }}</div>
-                                    </div>
+                        </v-expansion-panel-text>
+                    </v-expansion-panel>
+                </v-expansion-panels>
+            </v-card>
+        </div>
+        <div class="mt-4">
+            <v-card class="elevation-0">
+                <v-card-title class="d-flex align-center justify-space-between py-4">
+                    <h2 class="font-medium">Included Items</h2>
+                    <v-btn color="primary" size="small" icon @click="editItem({}, false)">
+                        <v-icon>mdi-plus</v-icon>
+                    </v-btn>
+                </v-card-title>
+    
+    
+                <v-card-text>
+                    <div v-if="travelPackage?.inclusions?.length">
+                        <v-list density="compact" class="mb-4">
+                            <v-list-item v-for="(item, index) in travelPackage?.inclusions" :key="'inc-' + item.id"
+                                class="py-4 position-relative border mb-4 rounded">
+                                <!-- Top right action buttons -->
+                                <div class="position-absolute top-0 right-0 mt-1 mr-1 d-flex">
+                                    <v-btn icon size="x-small" variant="tonal" color="primary"
+                                        @click="editItem(item, false)">
+                                        <v-icon>mdi-pencil</v-icon>
+                                    </v-btn>
                                 </div>
-                            </v-list-item-content>
-
-                        </v-list-item>
-                    </v-list>
-
-                </div>
-                <div v-else class="pl-4">
-                    <p>Items not added yet</p>
-                </div>
-            </v-card-text>
-
-        </v-card>
+    
+                                 <v-list-item-content>
+                                    <div class="d-flex align-start">
+                                        <v-icon color="success" size="20" class="mt-1 mr-3">
+                                            mdi-check-outline
+                                        </v-icon>
+    
+                                        <div>
+                                            <div class="font-weight-medium">{{ item.title }}</div>
+                                            <div class="text-body-2 text-medium-emphasis">{{ item.description }}</div>
+                                        </div>
+                                    </div>
+                                </v-list-item-content>
+    
+                            </v-list-item>
+                        </v-list>
+    
+    
+                    </div>
+                    <div v-else class="pl-4">
+                        <p>Items not added yet</p>
+                    </div>
+                </v-card-text>
+    
+            </v-card>
+        </div>
+        <div class="mt-4">
+            <v-card class="elevation-0">
+                <v-card-title class="d-flex align-center justify-space-between py-4">
+                    <h2 class="font-medium">Excluded Items</h2>
+                    <v-btn color="primary" size="small" icon @click="editItem({}, true)">
+                        <v-icon>mdi-plus</v-icon></v-btn>
+                </v-card-title>
+    
+    
+                <v-card-text>
+                    <div v-if="travelPackage?.exclusions?.length">
+                        <v-list density="compact" class="mb-4">
+                            <v-list-item v-for="(item, index) in travelPackage?.exclusions" :key="'inc-' + item.id"
+                                class="py-4 position-relative border mb-4 rounded">
+                                <!-- Top right action buttons -->
+                                <div class="position-absolute top-0 right-0 mt-1 mr-1 d-flex">
+                                    <v-btn icon size="x-small" variant="tonal" color="primary"
+                                        @click="editItem(item, true)">
+                                        <v-icon>mdi-pencil</v-icon>
+                                    </v-btn>
+                                </div>
+    
+                                <v-list-item-content>
+                                    <div class="d-flex align-start">
+                                        <v-icon color="error" size="20" class="mt-1 mr-3">
+                                            mdi-close-outline
+                                        </v-icon>
+    
+                                        <div>
+                                            <div class="font-weight-medium">{{ item.title }}</div>
+                                            <div class="text-body-2 text-medium-emphasis">{{ item.description }}</div>
+                                        </div>
+                                    </div>
+                                </v-list-item-content>
+    
+                            </v-list-item>
+                        </v-list>
+    
+                    </div>
+                    <div v-else class="pl-4">
+                        <p>Items not added yet</p>
+                    </div>
+                </v-card-text>
+    
+            </v-card>
+        </div>
+        <modal-template ref="globalModal" @saved="handleSaved" @close="handleClose"></modal-template>
     </div>
-    <modal-template ref="globalModal" @saved="handleSaved" @close="handleClose"></modal-template>
 </template>
 
 <script setup>
