fdcc85141562fcc14a4ac8043203e18f10e8bb28
DevLekh <devlekh@Mac-Mini.local>
2025-07-24 07:18:42 +0545

    wip

--- public/build/assets/CustomerPage-BdfuNpmr.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,j as o,o as r,d as s}from"./main-C2Wu7Inh.js";import"./index-DIycyeiE.js";const a={data(){return{}}};function n(i,e,c,p,u,d){return r(),o("div",null,e[0]||(e[0]=[s("p",null,"Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quidem.",-1)]))}const _=t(a,[["render",n]]);export{_ as default};
--- resources/admin/pages/packages/PackageForm-working.vue
+++ resources/admin/pages/packages/PackageForm-working.vue
@@ -13,8 +13,96 @@
 							</v-card-title>
 							<v-divider></v-divider>
 							<v-card-text class="pt-10">
-								<div>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, laborum?</p>
+								<v-row>
+									<v-col cols="12" md="12">
+										<div>
+											<v-text-field v-model="form.name" label="Package Name"
+												:rules="[rules.required]" :error-messages="errors.name"
+												density="comfortable" variant="outlined" :disabled="submitting"
+												required />
+										</div>
+									</v-col>
+									<v-col cols="12">
+										<div>
+											<v-text-field v-model="form.slug" label="URL" :rules="[rules.required]"
+												:error-messages="errors.slug" density="comfortable" variant="outlined"
+												:disabled="submitting" required />
+										</div>
+
+									</v-col>
+
+									<v-col cols="6">
+										<v-text-field v-model="form.duration_days" label="Duration (Days)"
+											density="comfortable" type="number" variant="outlined"
+											prepend-inner-icon="mdi-clock"
+											:rules="[rules.required, rules.numeric, rules.positive]"
+											:error-messages="errors.duration_days" :disabled="submitting" />
+									</v-col>
+
+									<v-col cols="6">
+										<v-text-field v-model="form.duration_nights" label="Duration (Nights)"
+											density="comfortable" type="number" variant="outlined"
+											prepend-inner-icon="mdi-clock"
+											:rules="[rules.required, rules.numeric, rules.positive]"
+											:error-messages="errors.duration_nights" :disabled="submitting" />
+									</v-col>
+
+									<v-col cols="6">
+										<v-text-field v-model="form.price" label="Price (USD)" type="number"
+											density="comfortable" prepend-inner-icon="mdi-currency-usd"
+											variant="outlined" :rules="[rules.required, rules.numeric, rules.positive]"
+											:error-messages="errors.price" :disabled="submitting" />
+									</v-col>
+
+									<v-col cols="6">
+										<v-text-field v-model="form.altitude" label="Altitude (feet)" type="number"
+											density="comfortable" prepend-inner-icon="mdi-image-filter-hdr"
+											variant="outlined" :rules="[rules.numeric, rules.positive]"
+											:error-messages="errors.altitude" :disabled="submitting" />
+									</v-col>
+
+									<v-col cols="6">
+										<v-date-input v-model="form.start_date" label="Start Date" prepend-icon=""
+											prepend-inner-icon="mdi-calendar" variant="outlined" density="comfortable"
+											:error-messages="errors.start_date" :disabled="submitting" />
+									</v-col>
+
+									<v-col cols="6">
+										<v-date-input v-model="form.end_date" label="End Date" prepend-icon=""
+											prepend-inner-icon="mdi-calendar" variant="outlined" density="comfortable"
+											:error-messages="errors.end_date" :disabled="submitting" />
+									</v-col>
+
+									<v-col cols="12">
+										<v-select v-model="form.category_ids" :items="package_categories"
+											item-title="name" item-value="id" label="Select Categories" multiple chips
+											clearable />
+									</v-col>
+									<v-col cols="6">
+										<v-switch v-model="form.is_active" label="Active" color="success" inset
+											:disabled="submitting" />
+									</v-col>
+
+									<v-col cols="6">
+										<v-switch v-model="form.is_featured" label="Featured" color="success" inset
+											:disabled="submitting" />
+									</v-col>
+
+
+								</v-row>
+								<div class="mb-4">
+									<label class="text-subtitle-1 mb-2 d-block">Description</label>
+									<RichTextEditor v-model="form.description" />
+									<span v-if="descriptionError" class="text-error text-caption">Content is
+										required</span>
+								</div>
+
+
+								<div class="mt-6 text-center">
+									<v-btn size="large" color="primary" rounded :loading="submitting"
+										:disabled="submitting" @click="submitPackage">
+										<v-icon left>mdi-check</v-icon> {{ package_id ? 'Update Package' : 'Create Package' }}
+									</v-btn>
 								</div>
 							</v-card-text>
 
@@ -23,15 +111,53 @@
 						</v-card>
 					</div>
 					<div class="mt-4" v-if="package_id">
-						<div>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, ullam?</p>
-						</div>
+						<v-card elevation="0" class="pa-6">
+							<v-card-title class="py-3">
+								<div>
+									<h5>Package Images
+									</h5>
+								</div>
+							</v-card-title>
+							<v-divider></v-divider>
+							<v-card-text class="pt-10">
+								<v-row>
+									<v-col cols="12" v-if="package_id">
+										<h4>Slider Images</h4>
+										<v-file-input prepend-icon="" v-model="selected_file" accept="image/*"
+											@change="handleUploadImage()" prepend-inner-icon="mdi-image"
+											label="File input"></v-file-input>
+										<div>
+											<v-row>
+												<v-col cols="6" md="4" v-for="(image, index) in form.images"
+													:key="index">
+													<div class="position-relative">
+														<img :src="image.url" alt="Image"
+															style="width: 100%; object-fit: contain;">
+
+														<!-- Delete icon -->
+														<v-icon color="red" small class="position-absolute"
+															style="top: 8px; right: 8px; cursor: pointer; padding: 2px;"
+															@click="handleDelete(image)" title="Delete Image">
+															mdi-close-thick
+														</v-icon>
+													</div>
+												</v-col>
+
+											</v-row>
+										</div>
+									</v-col>
+
+
+								</v-row>
+							</v-card-text>
+						</v-card>
 					</div>
 				</v-col>
 
 				<v-col cols="12" md="5" v-if="package_id">
 					<div class="mb-4">
-						<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis, maxime?</p>
+						<FormRight @close="fetchPackage" :travelPackage="travel_package"
+							:travelPackageId="package_id" />
 					</div>
 				</v-col>
 			</v-row>
