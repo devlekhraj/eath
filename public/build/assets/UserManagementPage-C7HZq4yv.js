99a54c2be249169d16678f3d374a58b37352621e
DevLekh <devlekh@Mac-Mini.local>
2025-07-24 09:07:05 +0545

    wip

---
+++ public/build/assets/UserManagementPage-C7HZq4yv.js
@@ -0,0 +1,1 @@
+import{_ as t,j as o,o as r,d as s}from"./main-CeqXzEwe.js";import"./index-DIycyeiE.js";const n={};function a(i,e){return r(),o("div",null,e[0]||(e[0]=[s("p",null,"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, ipsa.",-1)]))}const m=t(n,[["render",a]]);export{m as default};
