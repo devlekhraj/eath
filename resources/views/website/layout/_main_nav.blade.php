17ed9e0ce9cf40eedf09a039fe33e4d38bc4bf84
devlekhraj <devlekhraj88@gmail.com>
2025-08-21 01:23:58 +0545

    wip

--- resources/views/website/layout/_main_nav.blade.php
+++ resources/views/website/layout/_main_nav.blade.php
@@ -70,157 +70,7 @@
                                                 </ul>
                                             </div>
                                         @endif
-                                        {{-- <!-- Everest Region -->
-                                        <div class="col-md-3">
-                                            <h6 class="text-uppercase  mb-2 fw-bold">
-                                                Everest Region
-                                            </h6>
-                                            <ul class="list-unstyled">
-                                                <li><a href="#" class="dropdown-item">Everest Base Camp Trek -
-                                                        15 Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Mount Everest Base Camp
-                                                        Trek</a></li>
-                                                <li><a href="#" class="dropdown-item">Pikey Peak Trek - 9
-                                                        Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Footprint Special
-                                                        Everest
-                                                        BC Trek - 16 Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Everest Base Camp
-                                                        Luxury
-                                                        Trek - 13 Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Gokyo Lakes & Renjola
-                                                        Pass
-                                                        Trek - 15 Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Everest Panorama Trek
-                                                        - 8
-                                                        Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Everest BC Yoga Trek -
-                                                        16
-                                                        Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Everest Helicopter
-                                                        Trek -
-                                                        13 Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Everest Three Pass
-                                                        Trek -
-                                                        21 Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Everest Chola Pass
-                                                        Trek -
-                                                        18 Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Everest Base Camp Trek
-                                                        -
-                                                        12 Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Everest Base Camp via
-                                                        Salleri - 18 Days</a></li>
-                                            </ul>
-                                        </div>
 
-                                        <!-- Annapurna Region -->
-                                        <div class="col-md-3">
-                                            <h6 class="text-uppercase  mb-2 fw-bold">
-                                                Annapurna Region
-                                            </h6>
-                                            <ul class="list-unstyled">
-                                                <li><a href="#" class="dropdown-item">Tilicho Lake Trek - 9
-                                                        Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Ghorepani Poon Hill
-                                                        Trek -
-                                                        8 Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Mardi Himal Trek - 9
-                                                        Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Annapurna Circuit &
-                                                        Tilicho Lake Trek</a></li>
-                                                <li><a href="#" class="dropdown-item">Khopra Danda Trek - 10
-                                                        Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Nar-Phu Valley Trek -
-                                                        13
-                                                        Days</a></li>
-                                                <li><a href="#" class="dropdown-item">ABC with Poonhill - 13
-                                                        Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Annapurna Circuit Trek
-                                                        -
-                                                        13 Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Mardi Himal Base Camp
-                                                        Yoga
-                                                        Trek</a></li>
-                                                <li><a href="#" class="dropdown-item">ABC Trek - 8 Days</a>
-                                                </li>
-                                                <li><a href="#" class="dropdown-item">Annapurna Circuit
-                                                        Biking
-                                                        Trek - 14 Days</a></li>
-                                            </ul>
-                                        </div> --}}
-
-                                        <!-- Manaslu & Langtang Region -->
-                                        {{-- <div class="col-md-3">
-                                            <h6 class="text-uppercase  mb-2 fw-bold">
-                                                Manaslu Region
-                                            </h6>
-                                            <ul class="list-unstyled">
-                                                <li><a href="#" class="dropdown-item">Manaslu Circuit & Tsum
-                                                        Valley Trek - 21 Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Tsum Valley Trek - 14
-                                                        Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Manaslu Circuit Trek -
-                                                        15
-                                                        Days</a></li>
-                                            </ul>
-
-                                            <h6 class="text-uppercase  mt-4 mb-2 fw-bold">
-                                                Langtang Region
-                                            </h6>
-                                            <ul class="list-unstyled">
-                                                <li><a href="#" class="dropdown-item">Yala Peak Climbing -
-                                                        14
-                                                        Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Langtang Valley Trek -
-                                                        11
-                                                        Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Ruby Valley Trek - 12
-                                                        Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Langtang & Gosaikunda
-                                                        Trek
-                                                        - 16 Days</a></li>
-                                            </ul>
-                                        </div> --}}
-                                        {{-- 
-                                        <!-- Western & Eastern Region -->
-                                        <div class="col-md-3">
-                                            <h6 class="text-uppercase  mb-2 fw-bold">
-                                                Western Region
-                                            </h6>
-                                            <ul class="list-unstyled">
-                                                <li><a href="#" class="dropdown-item">Rara Lake Trek Package
-                                                        - 8
-                                                        Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Upper Dolpo Circuit
-                                                        Trek -
-                                                        26 Days</a></li>
-                                                <li><a href="#" class="dropdown-item">API Himal Base Camp
-                                                        Trek -
-                                                        17 Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Shey Phoksundo Lake
-                                                        Trek -
-                                                        11 Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Mustang Region Trek &
-                                                        Tours</a></li>
-                                                <li><a href="#" class="dropdown-item">Upper Mustang Jeep
-                                                        Tour -
-                                                        13 Days</a></li>
-                                                <li><a href="#" class="dropdown-item">Upper Mustang Trek -
-                                                        17
-                                                        Days</a></li>
-                                            </ul>
-
-                                            <h6 class="text-uppercase  mt-4 mb-2 fw-bold">
-                                                Eastern
-                                                Region
-                                            </h6>
-                                            <ul class="list-unstyled">
-                                                <li><a href="#" class="dropdown-item">Kanchenjunga
-                                                        Circuit/Base
-                                                        Camp Trek - 20 Days</a></li>
-                                            </ul>
-                                        </div> --}}
                                     </div>
                                 </div>
                             </div>
@@ -247,62 +97,7 @@
                                 @endforeach
                             @else
                             @endif
-                            {{-- <li>
-                                <a class="dropdown-item py-2 d-flex align-items-center" href="#">
-                                    <div style="width: 20px;">
-                                        <i class="fas fa-mountain "></i>
-                                    </div>
-                                    <span class="ms-2">Everest Heli Tour with Kalapatthar Landing</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item py-2 d-flex align-items-center" href="#">
-                                    <div style="width: 20px;">
-                                        <i class="fas fa-church "></i>
-                                    </div>
-                                    <span class="ms-2">Muktinath Helicopter Tour</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item py-2 d-flex align-items-center" href="#">
-                                    <div style="width: 20px;">
-                                        <i class="fas fa-water "></i>
-                                    </div>
-                                    <span class="ms-2">Gosaikunda Heli Tour</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item py-2 d-flex align-items-center" href="#">
-                                    <div style="width: 20px;">
-                                        <i class="fas fa-mountain-sun "></i>
-                                    </div>
-                                    <span class="ms-2">Mount Everest Heli Tour</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item py-2 d-flex align-items-center" href="#">
-                                    <div style="width: 20px;">
-                                        <i class="fas fa-tree "></i>
-                                    </div>
-                                    <span class="ms-2">Langtang Valley Heli Tour - Day Tour</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item py-2 d-flex align-items-center" href="#">
-                                    <div style="width: 20px;">
-                                        <i class="fas fa-hiking "></i>
-                                    </div>
-                                    <span class="ms-2">Mardi Himal Heli Tour</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item py-2 d-flex align-items-center" href="#">
-                                    <div style="width: 20px;">
-                                        <i class="fas fa-sun "></i>
-                                    </div>
-                                    <span class="ms-2">Annapurna Heli Tour</span>
-                                </a>
-                            </li> --}}
+                            
                         </ul>
 
 
@@ -321,11 +116,7 @@
                 </ul>
             </div>
             <div>
-                {{-- <div class="d-flex align-items-center d-md-flex">
-                    <i class="fas fa-user-circle me-2" style="font-size: 2rem"></i>
-                    <strong>Login</strong>
-
-                </div> --}}
+             
                 <!-- Navbar Toggler -->
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" style="border: 0"
--- resources/views/website/layout/offset-canva.blade.php
+++ resources/views/website/layout/offset-canva.blade.php
@@ -1,7 +1,17 @@
+<style>
+    .offcanvas .nav-item {
+        margin-bottom: 10px
+    }
+</style>
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasMenu" aria-labelledby="offcanvasMenuLabel">
 
     <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasMenuLabel">Menu</h5>
+        <div class="">
+            <a class="navbar-brand fw-bold text-primary fs-4" href="/">
+                <img src="/images/logo.png" alt="" height="70">
+            </a>
+            <h5 class="fw-bold text-primary mb-0" id="offcanvasMenuLabel">EATH Travel Pvt. Ltd</h5>
+        </div>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
@@ -9,55 +19,56 @@
 
             <!-- Home -->
             <li class="nav-item">
-                <a class="nav-link text-primary" href="#">Home</a>
+                <a href="/">Home</a>
             </li>
 
             <!-- Trekking in Nepal -->
             <li class="nav-item">
-                <a class="nav-link text-primary d-flex justify-content-between align-items-center"
-                    data-bs-toggle="collapse" href="#trekkingSubmenu" role="button" aria-expanded="false"
-                    aria-controls="trekkingSubmenu">
+                <a class="d-flex justify-content-between align-items-center" data-bs-toggle="collapse"
+                    href="#trekkingSubmenu" role="button" aria-expanded="false" aria-controls="trekkingSubmenu">
                     Trekking in Nepal
                     <i class="fa-solid fa-chevron-down ms-2 toggle-icon" data-target="#trekkingSubmenu"></i>
                 </a>
-                <div class="collapse" id="trekkingSubmenu">
-                    <ul class="nav flex-column ms-3">
-                        <li class="nav-item">
-                            <a class="nav-link text-secondary" href="#">Everest Region</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-secondary d-flex justify-content-between align-items-center"
-                                data-bs-toggle="collapse" href="#annapurnaSubmenu" role="button" aria-expanded="false"
-                                aria-controls="annapurnaSubmenu">
-                                Annapurna Region
-                                <i class="fa-solid fa-chevron-down ms-2 toggle-icon"
-                                    data-target="#annapurnaSubmenu"></i>
-                            </a>
-                            <div class="collapse" id="annapurnaSubmenu">
-                                <ul class="nav flex-column ms-3">
-                                    <li class="nav-item"><a class="nav-link text-secondary" href="#">Mardi
-                                            Himal</a></li>
-                                    <li class="nav-item"><a class="nav-link text-secondary" href="#">Poon
-                                            Hill</a></li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-secondary" href="#">Langtang Region</a>
-                        </li>
-                    </ul>
-                </div>
+                @if (count($trekkingInNepal->children) > 0)
+                    <div class="collapse" id="trekkingSubmenu">
+                        <ul class="nav flex-column ms-3">
+                            @foreach ($trekkingInNepal->children as $region)
+                                <li class="mb-3 ms-2">
+                                    <a class="text-primary" href="#"
+                                        style="font-weight: 500">{{ $region->name }}</a>
+                                    @if ($region->travelPackages->count() > 0)
+                                        <ul class="nav flex-column">
+                                            @foreach ($region->travelPackages as $pack)
+                                                <li class="d-flex">
+                                                    <div style="widows: 20px">
+                                                        <i class="fas fa-angle-right text-secondary me-2"></i>
+                                                    </div>
+                                                    <div>
+                                                        <a style="font-weight: 500; font-size:0.9rem"
+                                                            href="/packages/{{ $pack->slug }}"
+                                                            class="">{{ $pack->name }}</a>
+                                                    </div>
+                                                </li>
+                                            @endforeach
+                                        </ul>
+                                    @endif
+
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @else
+                @endif
             </li>
 
             <!-- Heli Tours -->
             <li class="nav-item">
-                <a class="nav-link text-primary d-flex justify-content-between align-items-center"
-                    data-bs-toggle="collapse" href="#heliToursSubmenu" role="button" aria-expanded="false"
-                    aria-controls="heliToursSubmenu">
+                <a class="d-flex justify-content-between align-items-center" data-bs-toggle="collapse"
+                    href="#heliToursSubmenu" role="button" aria-expanded="false" aria-controls="heliToursSubmenu">
                     Heli Tours
                     <i class="fa-solid fa-chevron-down ms-2 toggle-icon" data-target="#heliToursSubmenu"></i>
                 </a>
-                <div class="collapse" id="heliToursSubmenu">
+                {{-- <div class="collapse" id="heliToursSubmenu">
                     <ul class="nav flex-column ms-3">
                         <li class="nav-item"><a class="nav-link text-secondary" href="#">Everest Heli Tour</a>
                         </li>
@@ -66,7 +77,16 @@
                         <li class="nav-item"><a class="nav-link text-secondary" href="#">Gosaikunda Heli Tour</a>
                         </li>
                     </ul>
-                </div>
+                </div> --}}
+            </li>
+            <li class="nav-item">
+                <a href="/guide-profiles">Guide Profiles</a>
+            </li>
+            <li class="nav-item">
+                <a href="/blogs">Blogs</a>
+            </li>
+            <li class="nav-item">
+                <a href="/faq">FAQs</a>
             </li>
 
         </ul>
