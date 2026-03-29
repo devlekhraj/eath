15dc25c4503aeff9665c3272698a7a63bcbbeeea
devlekhraj <devlekhraj88@gmail.com>
2026-03-29 23:29:47 +0545

    fixes

--- public/build/assets/auth.api-C1sxFide.js
+++
@@ -1,1 +0,0 @@
-import{x as i}from"./main-CxHKihcN.js";function t(n){return i.post("/admin/login",n)}function p(){return i.get("/admin/profile")}export{t as l,p};
---
+++ public/build/assets/auth.api-DBBlYSpD.js
@@ -0,0 +1,1 @@
+import{x as i}from"./main-Dyx5WZUJ.js";function t(n){return i.post("/admin/login",n)}function p(){return i.get("/admin/profile")}export{t as l,p};
